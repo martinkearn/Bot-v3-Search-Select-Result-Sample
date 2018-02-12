ed7b984027dad83c3212c28d4e1d4439f0f39c66
Martin Kearn <martin.kearn@microsoft.com>
2018-02-12 15:16:15 +0000

    Initial dialog flow

--- CSharp/SearchSelectResultBot/Dialogs/RootDialog.cs
+++ CSharp/SearchSelectResultBot/Dialogs/RootDialog.cs
@@ -11,36 +11,21 @@ namespace SearchSelectResultBot.Dialogs
     {
         public async Task StartAsync(IDialogContext context)
         {
-            await context.PostAsync($"Enter a search term");
-
-            context.Wait(MessageReceivedAsync);
+            context.Wait(this.MessageReceivedAsync);
         }
 
-        private async Task MessageReceivedAsync(IDialogContext context, IAwaitable<object> result)
+        public virtual async Task MessageReceivedAsync(IDialogContext context, IAwaitable<object> result)
         {
-            var activity = await result as Activity;
-
-            //get search term. This might be an entity in LUIS bots using something lik
-            //var entity = result.Entities.FirstOrDefault(e => e.Type == "SeachTerm");
-            //var searchTerm = entity == null ? "" : entity.Entity;
-            var searchTerm = activity.Text;
-
-            //forward context to search results dialog. Pass search term to new dialog
-            await context.Forward(new SearchResultsDialog(searchTerm), this.ResumeAfterSearchResultsDialog, result, CancellationToken.None);
-
-            context.Wait(MessageReceivedAsync);
+            //Call SearchQueryDialog regardless of what the user says
+            //More typically this function would be a LUIS intent handler
+            context.Call(new SearchQueryDialog(), ResumeAfterSearchQueryDialog);
         }
 
-        private async Task ResumeAfterSearchResultsDialog(IDialogContext context, IAwaitable<object> result)
+        private async Task ResumeAfterSearchQueryDialog(IDialogContext context, IAwaitable<object> result)
         {
-            var chosenResultActivity = await result as IMessageActivity;
-
-            var chosenResult = chosenResultActivity.Text;
-
-            await context.PostAsync($"Chosen result {chosenResult}");
-
+            //just close the context and do nothing with the result
             context.Done("");
-
         }
+
     }
 }
---
+++ CSharp/SearchSelectResultBot/Dialogs/SearchQueryDialog.cs
@@ -0,0 +1,48 @@
+﻿using Microsoft.Bot.Builder.Dialogs;
+using Microsoft.Bot.Connector;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Web;
+
+namespace SearchSelectResultBot.Dialogs
+{
+    [Serializable]
+    public class SearchQueryDialog : IDialog<object>
+    {
+        protected int count = 1;
+
+        public async Task StartAsync(IDialogContext context)
+        {
+            await context.PostAsync("Enter a search term");
+            context.Wait(MessageReceivedAsync);
+        }
+
+        public virtual async Task MessageReceivedAsync(IDialogContext context, IAwaitable<IMessageActivity> argument)
+        {
+            var message = await argument;
+            var searchTerm = message.Text;
+
+            await context.PostAsync($"You searched for {searchTerm}");
+
+            context.Done("");
+        }
+
+        //public async Task AfterResetAsync(IDialogContext context, IAwaitable<bool> argument)
+        //{
+        //    var confirm = await argument;
+        //    if (confirm)
+        //    {
+        //        this.count = 1;
+        //        await context.PostAsync("Reset count.");
+        //    }
+        //    else
+        //    {
+        //        await context.PostAsync("Did not reset count.");
+        //    }
+        //    context.Wait(MessageReceivedAsync);
+        //}
+    }
+}
